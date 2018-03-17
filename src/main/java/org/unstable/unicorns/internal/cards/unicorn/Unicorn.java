4d900b9a205f52349ede727a32d6686bc46a5090
Ryan Roach <Ryan.Roach@cerner.com>
2018-03-16 19:26:46 -0500

    Update package declaration for Unicorn class and add images.

--- src/main/java/org/unstable/unicorns/internal/cards/unicorn/Unicorn.java
+++ src/main/java/org/unstable/unicorns/internal/cards/unicorn/Unicorn.java
@@ -1,10 +1,9 @@
-package org.unstable.unicorns;
+package org.unstable.unicorns.internal.cards.unicorn;
 
 /**
  * Description.
  * @author Your Name
  */
-public abstract class Unicorn
-{
+public abstract class Unicorn {
 
 }
