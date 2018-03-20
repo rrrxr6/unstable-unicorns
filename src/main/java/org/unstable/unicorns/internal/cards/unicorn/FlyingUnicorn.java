13e127bcbeae14e16e460bd6374ac6a9513c5cfd
Ryan Roach <Ryan.Roach@cerner.com>
2018-03-20 01:31:08 -0500

    Add flying unicorns

---
+++ src/main/java/org/unstable/unicorns/internal/cards/unicorn/FlyingUnicorn.java
@@ -0,0 +1,30 @@
+package org.unstable.unicorns.internal.cards.unicorn;
+
+import java.awt.Image;
+
+/**
+ * Description.
+ * @author Your Name
+ */
+public abstract class FlyingUnicorn extends Unicorn {
+
+    public FlyingUnicorn(String name, String description, Image image, Boolean hasStaticEffect) {
+        super(name, description, image, hasStaticEffect);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onSacrifice() {
+        // Player.addToHand(this)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onDestroy() {
+        // Player.addToHand(this)
+    }
+}
